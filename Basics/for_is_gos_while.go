bd513f229cd3a1c0901db4012a5f1eed7b93f815
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-06 11:14:18 -0700

    add if.go, if_with_a_short_statement.go, and for_is_gos_while.go

---
+++ Basics/for_is_gos_while.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+// At that point you can drop the semicolons: C's while is spelled for in Go.
+
+// If you omit the loop condition it loops forever, so an infinite loop is compactly expressed.
