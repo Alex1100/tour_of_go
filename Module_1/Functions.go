87db0b59803e8a127544eda7f2e34b40208600a2
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-05 23:32:15 -0700

    add variable_info.go and variables_with_initializers.go

--- Module_1/Functions.go
+++ Module_1/Functions.go
@@ -23,3 +23,12 @@ func add(x, y int) int {
 func main() {
 	fmt.Println("THE RESULT OF", addStringA(42), "+", addStringB(13), "IS", add(42, 13))
 }
+
+// A function can take zero or more arguments.
+
+// In this example, add takes two parameters of type int.
+
+// Notice that the type comes after the variable name.
+
+// (For more about why types look the way they do, see the article on Go's declaration syntax.)
+// https://blog.golang.org/gos-declaration-syntax
--- Module_1/multiple_results.go
+++ Module_1/multiple_results.go
@@ -12,3 +12,7 @@ func main() {
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
 }
+
+// A function can return any number of results.
+
+// The swap function returns two strings.
