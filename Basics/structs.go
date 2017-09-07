b9d4f660471fd90e09f53afbb18835fab80a98b4
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-06 20:27:17 -0700

    add structs.go

---
+++ Basics/structs.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	fmt.Prinln(Vertex{1, 2})
+}
+
+// Structs
+// A struct is a collection of fields.
+
+// (And a type declaration does what you'd expect.)
